fb7e3841bc499c275a0d21ce706e9e9e66e7de8e
161aegglor <80264949+161aegglor@users.noreply.github.com>
2024-09-20 09:50:53 +0200

    installation

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food, LLC</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-menu" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Food, LLC</a>
+            </div>
+
+            <!-- Collect the nav links for mobile view -->
+            <div class="collapse navbar-collapse" id="navbar-menu">
+                <ul class="nav navbar-nav visible-xs">
+                    <li><a href="#">Chicken</a></li>
+                    <li><a href="#">Beef</a></li>
+                    <li><a href="#">Sushi</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <!-- Page Heading -->
+    <div class="container text-center">
+        <h1>Our Menu</h1>
+    </div>
+
+    <!-- Tall Section -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 section-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Nulla consequat, odio nec dapibus malesuada, libero felis dapibus purus, a ultricies nunc justo sed sapien. Curabitur mollis metus quis ligula facilisis, id lacinia odio vestibulum. Integer tincidunt, felis eget interdum efficitur, magna eros condimentum enim, vel pellentesque felis urna non nulla. Vestibulum a justo magna. Vivamus cursus elit vel nulla sodales, ut tincidunt orci bibendum. Integer in felis ac ipsum tempus gravida a vitae nisi. Nullam semper auctor libero nec posuere. Praesent lobortis libero quis justo ornare fermentum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Nulla consequat, odio nec dapibus malesuada, libero felis dapibus purus, a ultricies nunc justo sed sapien. Curabitur mollis metus quis ligula facilisis, id lacinia odio vestibulum. Integer tincidunt, felis eget interdum efficitur, magna eros condimentum enim, vel pellentesque felis urna non nulla. Vestibulum a justo magna. Vivamus cursus elit vel nulla sodales, ut tincidunt orci bibendum. Integer in felis ac ipsum tempus gravida a vitae nisi. Nullam semper auctor libero nec posuere. Praesent lobortis libero quis justo ornare fermentum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Nulla consequat, odio nec dapibus malesuada, libero felis dapibus purus, a ultricies nunc justo sed sapien. Curabitur mollis metus quis ligula facilisis, id lacinia odio vestibulum. Integer tincidunt, felis eget interdum efficitur, magna eros condimentum enim, vel pellentesque felis urna non nulla. Vestibulum a justo magna. Vivamus cursus elit vel nulla sodales, ut tincidunt orci bibendum. Integer in felis ac ipsum tempus gravida a vitae nisi. Nullam semper auctor libero nec posuere. Praesent lobortis libero quis justo ornare fermentum.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
